7ad62802505e559290690bf8b0a9d5f851c92e5e
Scradarash <kaj.treep.1997@gmail.com>
2025-10-08 20:20:20 +0200

    Met behulp van AI de nieuwe Interfaces gemaakt, CategoryRepository gevuld met categorieen die bij de huidige producten passen en ze gelinkt. Het wordt nog nergens weergeven.

---
+++ Grocery.Core/Interfaces/Repositories/IProductCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Repositories
+{
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
+    {
+        List<ProductCategory> GetByProductId(int productId);
+        List<ProductCategory> GetByCategoryId(int categoryId);
+    }
+}
